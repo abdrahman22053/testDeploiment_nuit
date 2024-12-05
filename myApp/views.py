e93339154482e05a9d99cbc1f0d87db634bc9338
AbdoLegrand <abdototo4136@gmail.com>
2024-12-05 00:48:03 +0000

    Add andepoint

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Bienvenue à Nuit de l'Info!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>À propos de Nuit de l'Info</h1>")
+
--- myProject/settings.py
+++ myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0n6a0!s(-_(un9!q8#7$qz7p+issts&7paogiw(&ehefzyq2iv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myApp',
 ]
 
 MIDDLEWARE = [
